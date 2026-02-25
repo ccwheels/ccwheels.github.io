70928a53d8c31e3e5e72493c1d0d4acf9e08be5a
samjhett <sam@hettpaintinggroup.com>
2026-02-25 09:52:45 -0700

    Inject Google Tag Manager and Analytics scripts
    Added Google Tag Manager and Google Analytics injection scripts to load.js if not already present.

--- assets/includes/load.js
+++ assets/includes/load.js
@@ -1,6 +1,28 @@
 (function() {
   var path = 'includes/';
 
+  // Google Tag Manager - inject if not already present
+  if (!document.querySelector('script[src*="googletagmanager.com/gtm.js"]')) {
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-W79QLTNF');
+  }
+
+  // Google Analytics (gtag) - inject if not already present
+  if (!window.gtag) {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    window.gtag = gtag;
+    gtag('js', new Date());
+    gtag('config', 'G-CM9KT40C4X');
+    var ga = document.createElement('script');
+    ga.async = true;
+    ga.src = 'https://www.googletagmanager.com/gtm.js?id=G-CM9KT40C4X';
+    document.head.appendChild(ga);
+  }
+
   // Load HousecallPro booking script if not already present
   if (!document.querySelector('script[src*="housecallpro.com"]')) {
     var s = document.createElement('script');
